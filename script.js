cf704430767052ad58807b510e1199520c737d9b
Souphalack <100770535+Souphalack@users.noreply.github.com>
2023-03-30 23:18:52 +0800

    added greeting

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ square.addEventListener('click', (event) => changeColour ('green'));
 square.addEventListener('mouseover', (event) => changeColour('grey'));
 square.addEventListener ('mouseout', (event) => changeColour('red'));
 
+var words = document.getElementById('words');
+
+function greeting(){
+    words.innerHTML = "Welcome to Flatland <br> I am Square!"
+}
+
+document.onload = greeting()
 
 function createBuzzwordPhrase() {
     /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
